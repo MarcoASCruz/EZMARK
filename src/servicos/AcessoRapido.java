6413a591d27b749b4f9cddce66cbc970ebfd1941
MarcoCruz <marco.aurelio.s.cruz@gmail.com>
2015-05-30 20:55:19 -0300

    Testes com servico... A requisição POST está dando erro quando vai crair um factory no DAO e está incompleta...

--- src/servicos/AcessoRapido.java
+++ src/servicos/AcessoRapido.java
@@ -3,12 +3,15 @@ package servicos;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import modelos.AppResponse;
 import modelos.Favorito;
 
 import org.json.JSONObject;
@@ -18,16 +21,35 @@ public class AcessoRapido {
 	@GET
 	@Produces("application/json")
 	public Response getAll() {
-		
-		JSONObject jsonResult = new JSONObject();
-		Favorito f1 = new Favorito("TEste.com", "teste", "testeeeeeeeeeeeeee");
-		Favorito f2 = new Favorito("TEste2.com", "teste2", "testeeeeeeeeeeeeee2");
-		List<Favorito> favoritos = new ArrayList<Favorito>();
-		favoritos.add(f1);
-		favoritos.add(f2);
-		
-		jsonResult.put("favoritos", favoritos);
-		
-		return Response.status(200).entity(jsonResult.toString()).build();
+		AppResponse response = new AppResponse();
+		try{
+			response.setSuccess(true);
+			response.setContent("OK");
+		}
+		catch(Exception e){
+			response.setSuccess(false);
+			response.setContent(e.getMessage());
+			response.setStackTrace(e.getStackTrace());
+		}
+		return response.buildResponse();
 	}
+	
+	@POST
+	@Produces("application/json")
+	public Response adicionarItem() {
+		AppResponse response = new AppResponse();
+		try{
+			DAO.AcessoRapido acessoRapidoDAO = new DAO.AcessoRapido();
+			//acessoRapidoDAO.add(new Favorito());
+			response.setSuccess(true);
+			response.setContent("OK");
+		}
+		catch(Exception e){
+			response.setSuccess(false);
+			response.setContent(e.getMessage());
+			response.setStackTrace(e.getStackTrace());
+		}
+		return response.buildResponse();
+	}
+	
 }
