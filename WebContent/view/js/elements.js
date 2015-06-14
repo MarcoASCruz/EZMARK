d2f7d98c86398b0bd34da0bcc784acc3a5a6c91e
Allan <allandefan@gmail.com>
2015-06-13 23:50:17 -0300

    Novo elemento Bloco

--- WebContent/view/js/elements.js
+++ WebContent/view/js/elements.js
@@ -15,10 +15,48 @@ var ObjectHtml = function(){
 var Materialize = function () {
 	var self = this;
 
-	var Bloco = function (title){
+	var Bloco = function (icon, nomeFavorito, descricao, numEstrelas){
 		var bloco = new ObjectHtml();
 		bloco.createElement = function(){
-			return $('<div>');
+		var card = $('<div class="col s12 m3 pasta-fechada drag">');
+		var painel = $('<div class="card-panel card-complement cyan darken-2">');
+		var conteudo = $('<div class="card-content white-text">');
+		var div-icon = $('<div class=" col s4 pdzero">');
+		var icon = $('<img class="responsive-img" src="' + icon +  '">');
+		var div-dadosFavorito = $('<div class="col s8 cyan-text text-lighten-5 pdzero">');
+		var div-tituloFavorito = $('<div class="col s12 titulo-favorito">');
+		var titulo-favorito = $('<div class="col s10 truncate pdzero">');
+		var tituloFavorito = $('<i class="mdi-action-stars yellow-text text-accent-2"></i>' + nomeFavorito);
+		var div-opcoes = $('<div class="col s2">');
+		var btOpcoes = $('<a class="dropdown-button cyan-text text-lighten-5" data-activates="dropdown10">');
+		// Precisa adicionar as opções do dropown
+		var icon-btOpcoes = $('<i class="mdi-navigation-more-vert"></i>');
+		var div-estrelas = $('<div class="col s12">');
+		// for numero de estrelas	
+		var estrela = $('<i class="mdi-action-grade"></i>');
+		var div-descricao = $('<div class="col s12 truncate">');
+		var div-tags = $('<div class="col s12 truncate">');
+		var icon-tag = $('<i class="mdi-maps-local-offer"></i>');
+		
+		
+		card.append (painel);
+		card.append (conteudo);
+		conteudo.append (div-icon);
+		div-icon.append (icon);
+		conteudo.append (div-dadosFavorito);
+		div-dadosFavorito.append (div-tituloFavorito)
+		div-tituloFavorito.append (titulo-favorito)
+		titulo-favorito.append (tituloFavorito)
+		conteudo.append (div-opcoes);
+		div-opcoes.append (btOpcoes);
+		btOpcoes.append (icon-btOpcoes);
+		conteudo.append (div-estrelas);
+		div-estrelas.append (estrela);
+		conteudo.append (div-descricao);
+		div-descricao.append(descricao)
+		conteudo.append (div-tags);
+		div-tags.append (icon-tag); 
+			return card;
 		}
 		return bloco;
 	}
@@ -32,5 +70,8 @@ var materialize = new Materialize();
 var Element = {
 	Pasta: function(){ 
 		return new materialize.Pasta(); 
+	},
+	Bloco: function(){ 
+		return new materialize.Bloco(icon, nomeFavorito, descricao, numEstrelas); 
 	}
 }
