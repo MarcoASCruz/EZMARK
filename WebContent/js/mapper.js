f12c40741c4126312b7c9acbda640bbdbf250012
Marco Cruz <Marco Cruz@MrNotebook.Home>
2016-04-24 16:26:46 -0300

    realocando pastas

---
+++ WebContent/js/mapper.js
@@ -0,0 +1,488 @@
+var Mapper = function(){
+	this.modelos = {
+		//
+	}
+	
+	this.services = {
+		buscarArquivos: function(id, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/pasta/" + id
+				,
+				method: "GET"
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		buscarFavorito: function(id, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/favorito/" + id
+				,
+				method: "GET"
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		adicionarPasta: function(pasta, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/pasta"
+				,
+				method: "POST"
+				,
+				data:{
+					pasta: JSON.stringify(pasta)
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		alterarPasta: function(pasta, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/pasta"
+				,
+				method: "PUT"
+				,
+				data:{
+					pasta: JSON.stringify(pasta)
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		removerPasta: function(id, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/pasta"
+				,
+				method: "DELETE"
+				,
+				data:{
+					id: id
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		adicionarFavorito: function(favorito, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/favorito"
+				,
+				method: "POST"
+				,
+				data:{
+					favorito: JSON.stringify(favorito)
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		alterarFavorito: function(favorito, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/favorito"
+				,
+				method: "PUT"
+				,
+				data:{
+					favorito: JSON.stringify(favorito)
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		removerFavorito: function(id, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/favorito"
+				,
+				method: "DELETE"
+				,
+				data:{
+					id: id
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		removerArquivos: function(arquivos, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo"
+				,
+				method: "DELETE"
+				,
+				data:{
+					arquivos: JSON.stringify(arquivos)
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		atualizarAcesso: function(id, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/favorito/atualizarAcesso"
+				,
+				method: "PUT"
+				,
+				data:{
+					id: id
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		buscarAcessoRapido: function(onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/acessoRapido/buscarTodos"
+				,
+				method: "GET"
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		adicionarAcessoRapido: function(id, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/acessoRapido/adicionar"
+				,
+				method: "POST"
+				,
+				data:{
+					id: id
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		removerAcessoRapido: function(id, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/acessoRapido/remover"
+				,
+				method: "DELETE"
+				,
+				data:{
+					id: id
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		recemAcessados: function(onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/favorito/recemAcessados"
+				,
+				method: "GET"
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		maisAcessados: function(onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/favorito/maisAcessados"
+				,
+				method: "GET"
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		recemAdicionados: function(onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/favorito/recemAdicionados"
+				,
+				method: "GET"
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		buscarHierarquia: function(onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/pasta/hierarquia"
+				,
+				method: "GET"
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		uploadImagemFavorito: function(form, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/favorito/img"
+				,
+				type: "POST"
+				,	
+				contentType: false,       // The content type used when sending data to the server.
+				cache: false,             // To unable request pages to be cached
+				processData:false,        // To send DOMDocument or non processed data file it is set to false
+				data: form
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		uploadImagemPasta: function(form, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/pasta/img"
+				,
+				type: "POST"
+				,	
+				contentType: false,       // The content type used when sending data to the server.
+				cache: false,             // To unable request pages to be cached
+				processData:false,        // To send DOMDocument or non processed data file it is set to false
+				data: form
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}	
+		,
+		adicionarTagPasta: function(idPasta, tagNome, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/pasta/tag"
+				,
+				method: "POST"
+				,
+				data:{
+					idPasta: idPasta,
+					tagNome: tagNome
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		removerTagPasta: function(idPasta, idTag, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/pasta/tag"
+				,
+				method: "DELETE"
+				,
+				data:{
+					idPasta: idPasta,
+					idTag: idTag
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		adicionarTagFavorito: function(idFavorito, tagNome, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/favorito/tag"
+				,
+				method: "POST"
+				,
+				data:{
+					idFavorito: idFavorito,
+					tagNome: tagNome
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		removerTagFavorito: function(idFavorito, idTag, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/favorito/tag"
+				,
+				method: "DELETE"
+				,
+				data:{
+					idFavorito: idFavorito,
+					idTag: idTag
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		pesquisa: function(valorPesquisa, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/pesquisar/" + valorPesquisa
+				,
+				method: "GET"
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		logOut: function(onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/j_spring_security_logout"
+				,
+				method: "POST"
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		obterUsuario: function(onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/usuario/obterNome"
+				,
+				method: "GET"
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		adicionarUsuario: function(usuario, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/usuario/adicionar"
+				,
+				method: "POST"
+				,
+				data:{
+					usuario: JSON.stringify(usuario)
+				}
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		buscarHome: function(onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/usuario/buscarHome"
+				,
+				method: "GET"
+				,
+				success: onSuccess
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		compartilhar: function(idPasta, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/pasta/compartilhar"
+				,
+				method: "POST"
+				,
+				success: onSuccess
+				,
+				data:{
+					idPasta: idPasta
+				}
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+		,
+		moverArquivos: function(idPastaDestino, arquivos, onSuccess, onError){
+			requisicaoParametros = {
+				url: "/GerenciadorDeFavoritos/servicos/arquivo/mover"
+				,
+				method: "POST"
+				,
+				success: onSuccess
+				,
+				data:{
+					idPastaDestino: idPastaDestino
+					,
+					arquivos: JSON.stringify(arquivos)
+				}
+				,
+				error: onError
+			}
+			executarRequisicao(requisicaoParametros);
+		}
+	}
+	
+	var executarRequisicao = function(options){
+		$.ajax(options);
+	}
+	
+}
+var mapper = new Mapper();
+
---
+++ WebContent/js/servicos.js
@@ -0,0 +1,302 @@
+var servicos = {
+	buscarArquivos: function(idPasta, onSuccess){
+		mapper.services.buscarArquivos(
+			idPasta
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	buscarFavorito: function(idFavorito, onSuccess) {
+		mapper.services.buscarArquivos(
+			idFavorito
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	adicionarPasta: function(pasta, onSuccess) {
+		mapper.services.adicionarPasta(
+			pasta
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	alterarPasta: function(pasta, onSuccess) {
+		mapper.services.alterarPasta(
+			pasta
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	removerPasta: function(id, onSuccess) {
+		mapper.services.removerPasta(
+			id
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	adicionarTagPasta: function(idPasta, tagNome, onSuccess) {
+		mapper.services.adicionarTagPasta(
+			idPasta
+			,
+			tagNome
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	removerTagPasta: function(idPasta, idTag, onSuccess) {
+		mapper.services.removerTagPasta(
+			idPasta
+			,
+			idTag
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	adicionarFavorito: function(favorito, onSuccess) {
+		mapper.services.adicionarFavorito(
+			favorito
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	alterarFavorito: function(favorito, onSuccess) {
+		mapper.services.alterarFavorito(
+			favorito
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	removerFavorito: function(id, onSuccess) {
+		mapper.services.removerFavorito(
+			id
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	removerArquivos: function(arquivos, onSuccess) {
+		mapper.services.removerArquivos(
+			arquivos
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	adicionarTagFavorito: function(idFavorito, tagNome, onSuccess) {
+		mapper.services.adicionarTagFavorito(
+			idFavorito
+			,
+			tagNome
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	removerTagFavorito: function(idFavorito, idTag, onSuccess) {
+		mapper.services.removerTagFavorito(
+			idFavorito
+			,
+			idTag
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	atualizarAcesso: function(id, onSuccess) {
+		mapper.services.atualizarAcesso(
+			id
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	buscarAcessoRapido: function(onSuccess){
+		mapper.services.buscarAcessoRapido(
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	adicionarAcessoRapido: function(id, onSuccess){
+		mapper.services.adicionarAcessoRapido(
+			id
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	removerAcessoRapido: function(id, onSuccess){
+		mapper.services.removerAcessoRapido(
+			id
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	recemAcessados: function(onSuccess) {
+		mapper.services.recemAcessados(
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	maisAcessados: function(onSuccess) {
+		mapper.services.maisAcessados(
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	recemAdicionados: function(onSuccess) {
+		mapper.services.recemAdicionados(
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	buscarHierarquiaDePastas: function(onSuccess) {
+		mapper.services.buscarHierarquia(
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	//form, precisa ter a imagem e o id do favorito destino
+	uploadImagemFavorito: function(form, onSuccess) {
+		mapper.services.uploadImagemFavorito(
+			form
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	//form, precisa ter a imagem e o id do pasta destino
+	uploadImagemPasta: function(form, onSuccess) {
+		mapper.services.uploadImagemPasta(
+			form
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	pesquisa: function(form, onSuccess) {
+		mapper.services.pesquisa(
+			form
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	logOut: function(onSuccess) {
+		mapper.services.logOut(
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	obterUsuario: function(onSuccess) {
+		mapper.services.obterUsuario(
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	adicionarUsuario: function(usuario, onSuccess, onError) {
+		mapper.services.adicionarUsuario(
+			usuario
+			,
+			onSuccess
+			, 
+			onError
+		)
+	}
+	,
+	buscarHome: function(onSuccess) {
+		mapper.services.buscarHome(
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	compartilhar: function(idPasta, onSuccess) {
+		mapper.services.compartilhar(
+			idPasta
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+	,
+	moverArquivos: function(idPastaDestino, arquivos, onSuccess) {
+		mapper.services.moverArquivos(
+			idPastaDestino
+			,
+			arquivos
+			,
+			onSuccess
+			,
+			serviceShowError
+		)
+	}
+}
+function serviceShowError(data) {
+	var error = data.responseJSON;
+	Element.ToastError(error.content)
+	console.log(error);
+}
