41b0c8246c09d7453558d064306d47c3bb441496
Marco Cruz <Marco Cruz@MrNotebook.Home>
2016-04-24 16:06:45 -0300

    DTOArquivo

---
+++ src/modelos/DTOArquivo.java
@@ -0,0 +1,20 @@
+package modelos;
+
+public class DTOArquivo {
+	public int id;
+	public String tipo;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+}
